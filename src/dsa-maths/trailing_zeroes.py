25af4ba33cd8b199cd7c927a7cbafeeba3f233a1
Nishant <nishantnks@hotmail.com>
2022-01-17 14:21:41 +0530

    Implement effectient solution to count the number of 5's,25's,125's and so on in prime factorization.

--- src/dsa-maths/trailing_zeroes.py
+++ src/dsa-maths/trailing_zeroes.py
@@ -13,6 +13,16 @@ class TrailingZeroes:
         while factorial%10==0:
             count+=1
             factorial/=10
+        
+        """
+        Trailing zeroes are made by number of 5's and 2's in the prime factorization of number.
+        So we can count number of 5's since it's way less than number of 2's.
+        So we can use number of 5's , 25's ,125's and so on till the number is greater than next iteration of 5,25,125,625 and so on.
+        """
+        count,i=0,5
+        while i<=self.__number:
+            count+=int(self.__number/i)
+            i*=5
         return count
 
 
